ddad0f757920db0121b778081a6b32f26af3f590
G B <{ID}+{gehaimeBarros}@users.noreply.github.com>
2025-06-30 00:11:41 -0300

    REQ0493011123121232232-op00003

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contato from "./Contato";
 import Missao from "./pages/Missao";
@@ -13,5 +12,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
